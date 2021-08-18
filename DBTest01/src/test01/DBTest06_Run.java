4ae7c12b2b4772c21a1bb5d5c2e3c4955faa6557
jwcom1995 <95god95@gmail.com>
2021-08-18 21:01:52 +0900

    run JDBCPractice practice

---
+++ DBTest01/src/test01/DBTest06_Run.java
@@ -0,0 +1,14 @@
+package test01;
+
+public class DBTest06_Run {
+
+	public static void main(String[] args) {
+		DBTest06 test = new DBTest06();
+		test.selectAll();
+		//test.selectOne();
+		//test.insert();
+		//test.delete();
+		//test.update();
+	}
+
+}
